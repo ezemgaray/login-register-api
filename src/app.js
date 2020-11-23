908395eced4ccd6666b5df91026dd817c97a3e51
Ezequiel <ezemgaray@gmail.com>
2020-11-23 08:15:05 +0100

    Update app - Add auth routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express     = require('express')
 const config      = require('./config/config.js')
 const cors        = require('cors')
+const authRouter  = require('./router/auth.routes')
 
+// Create app
 const app = express()
 
 // Allow access only to slient domain
@@ -10,8 +12,10 @@ app.use(
     origin: config.clientDomain
   })
 )
-
 app.use(express.json())
 
+//Router
+app.use('/', authRouter)
 
+// Run server
 app.listen(config.port)
