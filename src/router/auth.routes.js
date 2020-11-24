9fecfb84f1924d2a0c42343d9febb6e459043b75
Ezequiel <ezemgaray@gmail.com>
2020-11-24 14:14:42 +0100

    Update auth.routes - Add /whoami endpoint

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -1,8 +1,12 @@
-const express         = require('express')
+const express         = require('express');
 const authController  = require('../controllers/auth.controller')
 const router          = express.Router()
+const authMiddleware  = require('../middlewares/auth.middleware')
 
 router.post('/register', authController.create)
 router.post('/login', authController.login)
+router.get('/whoami', authMiddleware, (req, res) => {
+  res.status(200).json(req.user)
+})
 
 module.exports = router
